2ec102ab95a4b51e807e11526fba4cead9e5cd51
pedroaz <pedro.azvm@gmail.com>
2021-01-23 14:47:39 -0300

    Screen Changer

---
+++ Assets/Managers/GamaInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamaInitializer : MonoBehaviour
+{
+    private ScreenManager screenManager;
+
+    private void Awake()
+    {
+        screenManager = FindObjectOfType<ScreenManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenManager.HideAll();
+        screenManager.GoToScene(0);
+    }
+}
---
+++ Assets/Managers/ScreenManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public List<GameObject> screens;
+    public int currentScreenNumber = 0;
+
+    public void HideAll()
+    {
+        foreach (var screen in screens) {
+            screen.SetActive(false);
+        }
+    }
+
+    public void GoToScene(int screenNumber)
+    {
+        HideAll();
+        currentScreenNumber = screenNumber;
+        screens[screenNumber].SetActive(true);
+    }
+}
